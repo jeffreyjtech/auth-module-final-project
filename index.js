8c86cc2e663ed5c3736320a5dae424169d783eff
Jeffrey Jenkins <jeffreyjtech@gmail.com>
2022-03-31 22:10:12 -0700

    Imported and added sync for contentDb

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 'use strict';
 
 const app = require('./src/server.js');
-const db = require('./src/auth/models');
+const { authDb } = require('./src/auth/models');
+const { contentDb } = require('./src/models');
 
 async function indexStart() {
-  await db.sync();
+  await authDb.sync();
+  await contentDb.sync();
   console.log('Database is synced');
   app.start(process.env.PORT || 3000);
 }
