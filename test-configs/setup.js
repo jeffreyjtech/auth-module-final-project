37a8099cc37167fbe9d473819a8d70f0009af760
Jeffrey Jenkins <jeffreyjtech@gmail.com>
2022-03-31 22:06:35 -0700

    Imported contentDb to be synced during setup

--- test-configs/setup.js
+++ test-configs/setup.js
@@ -1,9 +1,11 @@
 'use strict';
 
 const { authDb } = require('../src/auth/models');
+const { contentDb } = require('../src/models');
 
 module.exports = async () => {
   await authDb.sync();
+  await contentDb.sync();
   console.log('TEST Database is synced');
 };
   
