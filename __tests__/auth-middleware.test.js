26b5cd9ec04ec46039fdd8b2e51c1eaa9efbc656
Jeffrey Jenkins <jeffreyjtech@gmail.com>
2022-03-31 21:35:19 -0700

    Added tests for bearer route authentication/authorization

--- __tests__/auth-middleware.test.js
+++ __tests__/auth-middleware.test.js
@@ -6,14 +6,15 @@ const request = supertest(app);
 
 const base64 = require('base-64');
 
-describe('Auth testing', () => {
-  const username = 'test user';
-  const password = 'great passowrd';
+const username = 'test user';
+const password = 'great password';
+
+describe('Signin/signup testing', () => {
 
   test('Able to sign up', async () => {
-    const response = await request.post('/signup').send({username, password});
+    const response = await request.post('/signup').send({ username, password });
     expect(response.status).toBe(201);
-    expect(response.body.username).toBe(username);
+    expect(response.body.user.username).toBe(username);
   });
 
   test('Response is 403 on bad login', async () => {
@@ -28,22 +29,32 @@ describe('Auth testing', () => {
     const response = await request.post('/signin').set('authorization', `Basic ${authString}`);
 
     expect(response.status).toBe(200);
+    expect(response.body.user.token).toBeTruthy();
   });
 });
 
+describe('', () => {
+  
+  test('Unable to GET with non-admin user', async () => {
+    let authString = base64.encode(`${username}:${password}`);
 
+    let response = await request.post('/signin').set('authorization', `Basic ${authString}`);
 
+    let token = response.body.user.token;
 
+    response = await request.get('/users').set('authorization', `Bearer ${token}`);
 
+    expect(response.status).toBe(403);
+  });
 
+  test('Can GET with an admin account', async () => {
+    let response = await request.post('/signup').send({ username: 'admin', password: 'admin', role:'admin' });
 
+    let token = response.body.user.token;
 
+    response = await request.get('/users').set('authorization', `Bearer ${token}`);
 
-
-
-
-
-
-
-
-
+    expect(response.status).toBe(200);
+    expect(response.body[0]).toBeTruthy();
+  });
+});
