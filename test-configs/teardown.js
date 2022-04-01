6f3f848bcbdae59730e2f730820dac91b7e4b5b9
Jeffrey Jenkins <jeffreyjtech@gmail.com>
2022-03-31 22:06:21 -0700

    Imported contentDb to be dropped for teardown

--- test-configs/teardown.js
+++ test-configs/teardown.js
@@ -1,5 +1,9 @@
 'use strict';
 
 const { authDb } = require('../src/auth/models');
+const { contentDb } = require('../src/models');
 
-module.exports = () => authDb.drop();
+module.exports = () => {
+  authDb.drop();
+  contentDb.drop();
+};
