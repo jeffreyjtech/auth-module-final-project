b5a36336c905a8143799bda2681ecdbc9d071a0d
Jeffrey Jenkins <jeffreyjtech@gmail.com>
2022-04-03 16:19:07 -0700

    Added replies to models index.js

--- src/models/index.js
+++ src/models/index.js
@@ -3,14 +3,20 @@
 const { Sequelize, DataTypes } = require('sequelize');
 
 const threadModel = require('./thread-model.js');
+const replyModel = require('./reply-model.js');
 const Collection = require('./data-collection.js');
 
 const DATABASE_URL = process.env.DATABASE_URL || 'sqlite:.content';
 
 const sequelize = new Sequelize(DATABASE_URL);
 const threads = threadModel(sequelize, DataTypes);
+const replies = replyModel(sequelize, DataTypes);
+
+threads.hasMany(replies);
+replies.belongsTo(threads);
 
 module.exports = {
   contentDb: sequelize,
   threads: new Collection(threads), 
+  replies: new Collection(replies),
 };
