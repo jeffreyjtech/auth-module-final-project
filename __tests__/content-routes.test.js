2d1dc8e4eb1166be3679a3f140c6988b7e70b193
Jeffrey Jenkins <jeffreyjtech@gmail.com>
2022-04-03 16:16:52 -0700

    Created reply CRUD tests

--- __tests__/content-routes.test.js
+++ __tests__/content-routes.test.js
@@ -4,6 +4,8 @@ const supertest = require('supertest');
 const { app } = require('../src/server.js');
 const request = supertest(app);
 let token = '';
+let threadId = null;
+let replyId = null;
 
 describe('Test thread routes', () => {
   // Test logic goes here
@@ -13,9 +15,11 @@ describe('Test thread routes', () => {
     token = response.body.user.token;
 
     response = await request.post('/forum')
-      .send({creator: 'username', title: 'Why does new music suck?'})
+      .send({creator: 'editor', title: 'Why does new music suck?'})
       .set('authorization', `Bearer ${token}`);
     
+    threadId = response.body.id;
+
     expect(response.status).toBe(201);
   });
   test('Get all threads', async() => {
@@ -23,20 +27,76 @@ describe('Test thread routes', () => {
 
     expect(response.status).toBe(200);
     expect(response.body).toBeTruthy();
-    console.log(response.body);
   });
 
   test('update specific thread by id', async() => {
-    let response = await request.put(`/forum/${1}`).send({creator: 'username', title: 'New music is the best!!!1!1'}).set('authorization', `Bearer ${token}`);
+    let response = await request.put(`/forum/${threadId}`).send({creator: 'editor', title: 'New music is the best!!!1!1'}).set('authorization', `Bearer ${token}`);
 
     expect(response.status).toBe(201);
-    console.log(response.body);
     expect(response.body.title).toBe('New music is the best!!!1!1');
   });
 
   test('whether or not we can delete threads', async () => {
-    let response = await request.delete(`/forum/${1}`).set('authorization', `Bearer ${token}`);
+    let response = await request.delete(`/forum/${threadId}`).set('authorization', `Bearer ${token}`);
 
     expect(response.status).toBe(200);
+  });
+});
+
+describe('Test reply routes', () => {
+
+  test('Create a reply', async () => {
+    let response = await request.post('/forum')
+      .send({creator: 'editor', title: 'Why does new music suck?'})
+      .set('authorization', `Bearer ${token}`);
+
+    threadId = response.body.id;
+
+    response = await request.post(`/forum/${threadId}`)
+      .send({creator: 'editor', bodyText: 'I agree, new music does suck'})
+      .set('authorization', `Bearer ${token}`);
+    
+    expect(response.status).toBe(201);
+    expect(response.body.bodyText).toBe('I agree, new music does suck');
+  });
+
+  test('Get all replies', async () => {
+
+    let response = await request.post(`/forum/${threadId}`)
+      .send({creator: 'editor', bodyText: 'New music is the worst'})
+      .set('authorization', `Bearer ${token}`);
+
+    response = await request.get(`/forum/${threadId}`)
+      .set('authorization', `Bearer ${token}`);
+    
+    expect(response.status).toBe(200);
+    expect(response.body[0].bodyText).toBe('I agree, new music does suck');
+    expect(response.body[1].bodyText).toBe('New music is the worst');
+  
+    replyId = response.body[0].id;
+  });
+
+  test('Update a reply', async () => { 
+
+    let response = await request.put(`/forum/${threadId}/${replyId}`)
+      .send({creator: 'editor', bodyText: 'I changed my mind, I disagree and I like new music'})
+      .set('authorization', `Bearer ${token}`);
+
+    expect(response.status).toBe(201);
+    expect(response.body.bodyText).toBe('I changed my mind, I disagree and I like new music');
+  });
+
+  test('Delete a reply', async () => {
+
+    let response = await request.delete(`/forum/${threadId}/${replyId}`)
+      .set('authorization', `Bearer ${token}`);
+
+    expect(response.status).toBe(200);
+
+    response = await request.get(`/forum/${threadId}`)
+      .set('authorization', `Bearer ${token}`);
+
+    expect(response.body.find(reply => reply.bodyText === 'I agree, new music does suck')).toBeFalsy();
+    expect(response.body.find(reply => reply.bodyText === 'New music is the worst')).toBeTruthy();
   });
 });
