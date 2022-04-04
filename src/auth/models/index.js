56c731db6270d8045f4b40f65fa9a2500154b2ce
Beau Hibbert <beauhibbert789@gmail.com>
2022-04-03 18:51:49 -0700

    changed heroku postgres url

--- src/auth/models/index.js
+++ src/auth/models/index.js
@@ -4,9 +4,9 @@ const { Sequelize, DataTypes } = require('sequelize');
 
 const userModel = require('./users-model');
 
-const HEROKU_POSTGRESQL_MAROON_URL = process.env.HEROKU_POSTGRESQL_MAROON_URL || 'sqlite:.auth';
+const HEROKU_POSTGRESQL_BLUE_URL = process.env.HEROKU_POSTGRESQL_BLUE_URL || 'sqlite:.auth';
 
-const authSequelize = new Sequelize(HEROKU_POSTGRESQL_MAROON_URL);
+const authSequelize = new Sequelize(HEROKU_POSTGRESQL_BLUE_URL);
 
 module.exports = {
   authDb: authSequelize,
